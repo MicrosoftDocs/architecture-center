184fbfbbf840943e7e811296c9058efe740a567e
Christopher Bennage <bennage@microsoft.com>
2018-02-20 11:50:58 -0800

    hybrid networking

--- build/build.js
+++ build/build.js
@@ -2,3 +2,4 @@ const formatIndexFor = require('./reference-architectures/index');
 formatIndexFor('.');
 formatIndexFor('app-service-web-app');
 formatIndexFor('dmz');
+formatIndexFor('hybrid-networking');
