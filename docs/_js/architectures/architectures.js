cbc404ffd5b00b48f12fc681a17445a2f1c846a9
Adam Boeglin <adboegli@microsoft.com>
2019-12-16 10:38:43 -0800

    Support resource fix

--- docs/_js/architectures/architectures.js
+++ docs/_js/architectures/architectures.js
@@ -296,7 +296,7 @@ function filter(pageNumber, newSearch=false) {
     }
 
     // Load content data and filter it
-    $.getJSON('/azure/architecture/solution-ideas/data/output.json.txt', function (data) {
+    $.getJSON('/azure/architecture/solution-ideas/data/output.json', function (data) {
         var filterTerms = [];
         var selectedNames = [];
         var selectedCategories = [];
@@ -317,7 +317,7 @@ function filter(pageNumber, newSearch=false) {
                     '<button type="button" aria-label="Remove "' + checkName +
                     '" name="' +  checkName + '" class="delete" onclick="unCheck(\'' + checkId +
                     '\')"></button></span>');
-                filterTerms = filterTerms.concat($(this).val().split(" "));
+                filterTerms = filterTerms.concat($(this).val().split(" ").toLowerCase());
             });
         };
        
@@ -346,7 +346,7 @@ function filter(pageNumber, newSearch=false) {
             selectedNames.push($(this).attr('friendly-name'));
         });
 
-        $.getJSON('/azure/architecture/solution-ideas/metadata/display-tags.json.txt', function (tagData) {
+        $.getJSON('/azure/architecture/solution-ideas/metadata/display-tags..json', function (tagData) {
             // Get the tags for every checked item
             var visibleArticleTags = Array.from(new Set([].concat.apply([], data['articles'].map(data => data['tags'])).sort()))
 
